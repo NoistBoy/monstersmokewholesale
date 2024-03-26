ada26516a995408806e44a4a488fcea1c5015c64
Gmurtazak <murtazakasmanis@gmail.com>
2024-03-26 22:55:10 +0500

    website update

---
+++ js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $('.dropdown').hover(function () {
+        $(this).addClass('show');
+        $(this).find('.dropdown-menu').addClass('show');
+    }, function () {
+        $(this).removeClass('show');
+        $(this).find('.dropdown-menu').removeClass('show');
+    });
+ 
+
+      $('.sidebar li').click(function(){
+ 
+        var id = $(this).attr('id');
+        var category_list = $(this).closest('#category-list');
+        category_list.hide();
+        var list_values_to_show = $('#' + id + '-list');
+        list_values_to_show.fadeIn();
+
+        list_values_to_show.on('click', '.back-to-category', function() {
+          category_list.fadeIn();
+          list_values_to_show.hide();
+          
+        });
+ 
+      });
+ 
+    
+});
+ 
+$('.ads_sponsors').slick({
+  autoplay: true,
+  autoplaySpeed: 1000,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  arrows: true 
+});
+
+function toggleSidebar() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+}
