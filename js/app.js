75077ccd162887ee334ebb2ff0fd981be937ad51
NoistBoy <105210327+NoistBoy@users.noreply.github.com>
2024-03-29 00:58:07 +0500

    new changes

--- js/app.js
+++ js/app.js
@@ -1,34 +1,37 @@
 $(document).ready(function () {
-    $('.dropdown').hover(function () {
-        $(this).addClass('show');
-        $(this).find('.dropdown-menu').addClass('show');
-    }, function () {
-        $(this).removeClass('show');
-        $(this).find('.dropdown-menu').removeClass('show');
+  $('.dropdown').hover(function () {
+    $(this).addClass('show');
+    $(this).find('.dropdown-menu').addClass('show');
+  }, function () {
+    $(this).removeClass('show');
+    $(this).find('.dropdown-menu').removeClass('show');
+  });
+
+
+  $('.sidebar li').click(function () {
+
+    var id = $(this).attr('id');
+    var category_list = $(this).closest('#category-list');
+    category_list.hide();
+    var list_values_to_show = $('#' + id + '-list');
+    list_values_to_show.fadeIn();
+
+    list_values_to_show.on('click', '.back-to-category', function () {
+      category_list.fadeIn();
+      list_values_to_show.hide();
+
     });
- 
 
-      $('.sidebar li').click(function(){
- 
-        var id = $(this).attr('id');
-        var category_list = $(this).closest('#category-list');
-        category_list.hide();
-        var list_values_to_show = $('#' + id + '-list');
-        list_values_to_show.fadeIn();
+  });
+  $('.top-5-products').click(function (e) {
+    e.preventDefault();
 
-        list_values_to_show.on('click', '.back-to-category', function() {
-          category_list.fadeIn();
-          list_values_to_show.hide();
-          
-        });
- 
-      });
- 
-    
+  });
+
 });
- 
+
 
 function toggleSidebar() {
-    var sidebar = document.querySelector('.sidebar');
-    sidebar.classList.toggle('active');
+  var sidebar = document.querySelector('.sidebar');
+  sidebar.classList.toggle('active');
 }
